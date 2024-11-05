3645da22adece040fe1c7c76176a446824f8da68
jroznowski <42657416+jroznowski@users.noreply.github.com>
2024-11-05 22:50:45 +0100

    lab4 update bst

--- lab4/sorting/NodeT.cs
+++ lab4/sorting/NodeT.cs
@@ -8,9 +8,14 @@ namespace sorting
 {
     internal class NodeT
     {
-        public NodeT lewe;
-        public NodeT prawe;
-        public NodeT rodzic;
+        public NodeT? lewe;
+        public NodeT? prawe;
+        public NodeT? root;
         public int data;
+
+        public NodeT(int data)
+        {
+            this.data = data;
+        }
     }
 }
--- lab4/sorting/Tree.cs
+++ lab4/sorting/Tree.cs
@@ -8,6 +8,71 @@ namespace sorting
 {
     internal class Tree
     {
-        Node korzen;
+        NodeT root;
+
+        public void InsertNode(NodeT node)
+        {
+            if (this.root == null)
+            {
+                this.root = node;
+            }
+            else
+            {
+                Insert(this.root, node);
+            }
+        }
+
+        public void Insert(NodeT root, NodeT node)
+        {
+            if (node.data < root.data)
+            {
+                if (root.lewe == null)
+                {
+                    root.lewe = node;
+                }
+                else
+                {
+                    Insert(root.lewe, node);
+                }
+
+            }
+            else if (node.data > root.data)
+            {
+                if (root.prawe == null)
+                {
+                    root.prawe = node;
+                }
+                else
+                {
+                    Insert(root.prawe, node);
+                }
+            }
+        }
+
+        public void InOrder(NodeT root)
+        {
+            if (root.lewe != null)
+            {
+                InOrder(root.lewe);
+            }
+            Console.WriteLine(root.data);
+            if (root.prawe != null)
+            {
+                InOrder(root.prawe);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Tree drzewo = new Tree();
+            drzewo.InsertNode(new NodeT(4));
+            drzewo.InsertNode(new NodeT(2));
+            drzewo.InsertNode(new NodeT(1));
+            drzewo.InsertNode(new NodeT(6));
+            drzewo.InsertNode(new NodeT(3));
+            drzewo.InsertNode(new NodeT(7));
+            drzewo.InsertNode(new NodeT(5));
+            drzewo.InOrder(drzewo.root);
+        }
     }
 }
+
