cd50130f63c122f745554ed7a21796a366cd0a24
jroznowski <42657416+jroznowski@users.noreply.github.com>
2024-11-12 22:04:05 +0100

    lab4 remove function add

--- lab4/sorting/Tree.cs
+++ lab4/sorting/Tree.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -49,10 +50,41 @@ namespace sorting
             }
         }
 
-        /*public void Remove(NodeT node)
+        public NodeT Remove(NodeT node, int key)
         {
+            if(node == null)
+            {
+                return node;
+            }
+            if(key > node.data)
+            {
+                node.prawe = this.Remove(node.prawe, key);
+            }
+            else if(key < node.data){
+                node.lewe = this.Remove(node.lewe, key);
+            }
+            else
+            {
+                if(node.lewe == null)
+                {
+                    return node.prawe;
+                }
+                else if(node.prawe == null)
+                {
+                    return node.lewe;
+                }
 
-        }*/
+                NodeT temp = node.prawe;
+                while (temp.lewe != null)
+                {
+                    temp = temp.lewe;
+                }
+                node.data = temp.data;
+                node.prawe = this.Remove(node.prawe, temp.data);
+            }
+            return node;
+
+        }
 
         public void InOrder(NodeT root)
         {
@@ -76,6 +108,9 @@ namespace sorting
             drzewo.AddNode(new NodeT(3));
             drzewo.AddNode(new NodeT(7));
             drzewo.AddNode(new NodeT(5));
+            drzewo.InOrder(drzewo.root);
+            drzewo.Remove(drzewo.root, 3);
+            Console.WriteLine("========================");
             drzewo.InOrder(drzewo.root);
         }*/
     }
